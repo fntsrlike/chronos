933e0c20f1a952f29faedfe8fba86f4b55458132
Yuehu Lin <dev@fntsr.tw>
2023-04-10 11:20:24 +0800

    fix(gapi): remove useless scope for reading emails

--- src/composables/useGoogle.ts
+++ src/composables/useGoogle.ts
@@ -17,7 +17,6 @@ const SCOPES = [
   'https://www.googleapis.com/auth/spreadsheets',
   'https://www.googleapis.com/auth/userinfo.email',
   'https://www.googleapis.com/auth/userinfo.profile',
-  'https://www.googleapis.com/auth/user.emails.read',
 ];
 
 const gapiLoaded = ref(false);
