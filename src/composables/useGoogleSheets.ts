ea6aaa420e1b27a78c25c528e876bcad5ea3c9e3
Yuehu Lin <dev@fntsr.tw>
2023-12-26 02:23:50 +0800

    refactor(report): extract data generator from sheet

--- src/composables/useGoogleSheets.ts
+++ src/composables/useGoogleSheets.ts
@@ -1,12 +1,6 @@
-import { unref } from 'vue';
-import { DateTime } from 'luxon';
-import { storeToRefs } from 'pinia';
-import { useSettingsStore } from 'src/stores/settings-store';
 import { useLocalStorage } from '@vueuse/core';
 import { useReportGenerator } from 'src/composables/useReportGenerator';
 import { useGoogle } from './useGoogle';
-import { useGoogleProfiles } from './useGoogleProfiles';
-import { useTimeCalculator } from './useTimeCalculator';
 
 const spreadsheetId = useLocalStorage('spreadsheetId', '');
 const sheetName = useLocalStorage('sheetName', '');
@@ -19,35 +13,12 @@ const appendData = async (_gapi : typeof gapi) => {
     insertDataOption: 'INSERT_ROWS',
   };
 
-  const { minDate, maxDate } = storeToRefs(useSettingsStore());
-  const { recordType, teamName } = useReportGenerator();
-
-  const { getProfiles } = useGoogleProfiles();
-  const { name } = getProfiles();
-
-  const {
-    workHours, leaveHours, supportHours, meetingHours, improvingHours,
-    devHours, focusDevHours, brokenDevHours,
-    devWithoutImprovingHours, focusDevWithoutImprovingHours, brokenDevWithoutImprovingHours,
-  } = useTimeCalculator();
-
-  const from = minDate.value.toFormat('yyyy-LL-dd');
-  const to = maxDate.value.toFormat('yyyy-LL-dd');
-  const createdAt = DateTime.now().toFormat('yyyy-LL-dd TT');
-
-  const data = [
-    from, to, recordType,
-    teamName, name,
-    workHours, leaveHours, supportHours, meetingHours, improvingHours,
-    devHours, focusDevHours, brokenDevHours,
-    devWithoutImprovingHours, focusDevWithoutImprovingHours, brokenDevWithoutImprovingHours,
-    createdAt,
-  ].map((d) => (unref(d)));
-
+  const { dataRow } = useReportGenerator();
   const valueRangeBody = {
     majorDimension: 'ROWS',
-    values: [data],
+    values: [dataRow.value],
   };
+
   await _gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
 };
 
--- src/composables/useReportGenerator.ts
+++ src/composables/useReportGenerator.ts
@@ -1,13 +1,43 @@
 import { useLocalStorage } from '@vueuse/core';
 import { computed, ref } from 'vue';
+import { DateTime } from 'luxon';
+import { storeToRefs } from 'pinia';
+import { useSettingsStore } from 'src/stores/settings-store';
+import { useGoogleProfiles } from './useGoogleProfiles';
+import { useTimeCalculator } from './useTimeCalculator';
 
 const teamName = useLocalStorage('teamName', 'awesome team');
 
 const isForecast = ref(true);
 const recordType = computed(() => (isForecast.value ? 'forecast' : 'actual'));
 
+const { minDate, maxDate } = storeToRefs(useSettingsStore());
+
+const { getProfiles } = useGoogleProfiles();
+const { name } = getProfiles();
+
+const {
+  workHours, leaveHours, supportHours, meetingHours, improvingHours,
+  devHours, focusDevHours, brokenDevHours,
+  devWithoutImprovingHours, focusDevWithoutImprovingHours, brokenDevWithoutImprovingHours,
+} = useTimeCalculator();
+
+const from = minDate.value.toFormat('yyyy-LL-dd');
+const to = maxDate.value.toFormat('yyyy-LL-dd');
+const createdAt = DateTime.now().toFormat('yyyy-LL-dd TT');
+
+const dataRow = computed(() => [
+  from, to, recordType.value,
+  teamName.value, name,
+  workHours.value, leaveHours.value, supportHours.value, meetingHours.value, improvingHours.value,
+  devHours.value, focusDevHours.value, brokenDevHours.value,
+  devWithoutImprovingHours.value, focusDevWithoutImprovingHours.value, brokenDevWithoutImprovingHours.value,
+  createdAt,
+]);
+
 export const useReportGenerator = () => ({
   teamName,
   isForecast,
   recordType,
+  dataRow,
 });
