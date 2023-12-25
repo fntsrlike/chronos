8306fbc51efbdfaaca48c8a7e69a85352d00492f
Yuehu Lin <dev@fntsr.tw>
2023-12-26 00:21:51 +0800

    feat(toolbar): show button only when authenticated

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,77 +7,60 @@
         <q-toolbar-title>
           Chronos
         </q-toolbar-title>
-        <q-btn
-          flat
-          round
-          icon="send"
-          v-if="isAuthenticated"
-        >
-          <SendData/>
-        </q-btn>
 
-        <q-btn
-          flat
-          round
-          icon="disabled_visible"
-          :text-color="showDeclinedEvent ? '' : 'grey'"
-          :disable="isLoading"
-          @click="toggleShowDeclined"
-        >
-          <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
-            Show declined events
-          </q-tooltip>
-        </q-btn>
+        <template v-if="isAuthenticated">
+          <q-btn
+            flat
+            round
+            icon="send"
+          >
+            <SendData/>
+          </q-btn>
 
-        <q-btn
-          flat
-          round
-          icon="refresh"
-          :loading="isLoading"
-          @click="updateEvents"
-          v-if="isAuthenticated"
-        >
-          <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
-            Reload Events
-          </q-tooltip>
-          <template v-slot:loading>
-            <q-spinner/>
-          </template>
-        </q-btn>
+          <q-btn
+            flat
+            round
+            icon="disabled_visible"
+            :text-color="showDeclinedEvent ? '' : 'grey'"
+            :disable="isLoading"
+            @click="toggleShowDeclined"
+          >
+            <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
+              Show declined events
+            </q-tooltip>
+          </q-btn>
 
-        <q-btn
-          flat
-          round
-          icon="account_circle"
-          text-color="grey"
-          :loading="isLoading"
-          @click="updateEvents"
-          v-if="!isAuthenticated"
-        >
-          <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
-            Sign in with Google
-          </q-tooltip>
-          <template v-slot:loading>
-            <q-spinner/>
-          </template>
-        </q-btn>
+          <q-btn
+            flat
+            round
+            icon="refresh"
+            :loading="isLoading"
+            @click="updateEvents"
+          >
+            <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
+              Reload Events
+            </q-tooltip>
+            <template v-slot:loading>
+              <q-spinner/>
+            </template>
+          </q-btn>
 
-        <q-btn
-          flat
-          round
-          @click="dialog = true"
-          v-if="isAuthenticated"
-        >
-          <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
-            Sign In
-          </q-tooltip>
-          <q-avatar size="24px">
-            <img :src="avatarUrl">
-          </q-avatar>
-          <template v-slot:loading>
-            <q-spinner/>
-          </template>
-        </q-btn>
+          <q-btn
+            flat
+            round
+            @click="dialog = true"
+          >
+            <q-tooltip  anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
+              Setting
+            </q-tooltip>
+            <q-avatar size="24px">
+              <img :src="avatarUrl">
+            </q-avatar>
+            <template v-slot:loading>
+              <q-spinner/>
+            </template>
+          </q-btn>
+        </template>
       </q-toolbar>
     </q-header>
 
