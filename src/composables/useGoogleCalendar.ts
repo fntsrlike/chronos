c09e166e356a01dd0f89ddf44ec7d191693bd740
Yuehu Lin <dev@fntsr.tw>
2023-12-25 17:09:13 +0800

    refactor(google): use gapi by passing (getEvent)

--- src/composables/useGoogleCalendar.ts
+++ src/composables/useGoogleCalendar.ts
@@ -1,7 +1,6 @@
 import { storeToRefs } from 'pinia';
 import { useSettingsStore } from 'src/stores/settings-store';
 import { Ref, ref } from 'vue';
-import { Notify } from 'quasar';
 import { IEvent } from '../interfaces/event';
 import { useGoogle } from './useGoogle';
 
@@ -12,39 +11,28 @@ const {
   minDate, maxDate, email,
 } = storeToRefs(useSettingsStore());
 
-const getEvents = async () => {
-  try {
-    const response = await window.gapi.client.calendar.events.list({
-      calendarId: 'primary',
-      timeMin: minDate.value.toISO(),
-      timeMax: maxDate.value.toISO(),
-      showDeleted: false,
-      singleEvents: true,
-    });
+const getEvents = async (_gapi: typeof gapi) => {
+  const response = await _gapi.client.calendar.events.list({
+    calendarId: 'primary',
+    timeMin: minDate.value.toISO(),
+    timeMax: maxDate.value.toISO(),
+    showDeleted: false,
+    singleEvents: true,
+  });
 
-    events.value = response.result.items.filter((event) => {
-      if (showDeclinedEvent.value) {
-        return true;
-      }
-      const attendeeResponse = event.attendees?.filter((attde) => attde.email === email.value).at(0);
-      return (attendeeResponse?.responseStatus !== 'declined');
-    }).map((e) => e as IEvent);
-  } catch {
-    Notify.create({
-      icon: 'error',
-      message: 'Google API error!',
-      caption: 'Try again later',
-      color: 'negative',
-      position: 'top-right',
-      progress: true,
-    });
-  }
+  events.value = response.result.items.filter((event) => {
+    if (showDeclinedEvent.value) {
+      return true;
+    }
+    const attendeeResponse = event.attendees?.filter((attendee) => attendee.email === email.value).at(0);
+    return (attendeeResponse?.responseStatus !== 'declined');
+  }).map((e) => e as IEvent);
 };
 
-const { checkToken, isAuthenticated } = useGoogle();
+const { callGapi, isAuthenticated } = useGoogle();
 
 const updateEvents = () => {
-  checkToken(getEvents);
+  callGapi(getEvents);
 };
 
 const updateEventsIfAuthed = () => {
